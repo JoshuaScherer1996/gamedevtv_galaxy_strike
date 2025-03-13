ae6dd5de0b8b5c5a1312ae69381595f889565100
Josh <Jscherer2011@gmx.de>
2025-03-13 21:31:10 +0100

    added clamp for the player movement inside the PlayerMovements.cs script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed = 50f;
+    [SerializeField] private float xClampRange = 25f;
+    [SerializeField] private float yClampRange = 15f;
 
     Vector2 movement;
 
@@ -20,7 +22,14 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffset = movement.x * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
+
         float yOffset = movement.y * controlSpeed * Time.deltaTime;
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffset, transform.localPosition.y + yOffset, 0f);
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yClampRange, yClampRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 }
