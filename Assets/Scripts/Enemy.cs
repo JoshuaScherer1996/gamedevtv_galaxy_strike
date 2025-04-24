8d975417e31fe6318fc3ada1c0bf19927ea9fd32
Josh <Jscherer2011@gmx.de>
2025-04-24 20:58:46 +0200

    created the ProcessHit method inside the Enemy.cs script to handle the life calculation and hit event logic

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
+    [SerializeField] int hitPoints = 4;
 
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
     }
 }
