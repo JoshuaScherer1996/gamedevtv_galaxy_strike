b9f373c44326ad53cb49a8bfc635385cea674ab1
Josh <Jscherer2011@gmx.de>
2025-11-13 22:23:50 +0100

    added the scoreboard UI Text

--- Assets/Scripts/Scoreboard.cs
+++ Assets/Scripts/Scoreboard.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
+    [SerializeField] TMP_Text scoreboardText;
     private int score = 0;
 
     public void IncreaseScore(int amount)
     {
         score += amount;
-        Debug.Log(score);
+        scoreboardText.text = score.ToString();
     }
 }
